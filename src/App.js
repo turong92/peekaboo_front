bd5e7674ab3463b2cd499d47128e85c0564dd269
turong92 <rkclrhks33@gmail.com>
2021-05-18 23:26:12 +0900

    ShouldComponentUpdate까지

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ axios.get('/test')
 class App extends Component {
   constructor(props){
     super(props);
+    this.max_content_id = 3;
     this.state = {
       mode:"read",
       selected_content_id:2,
@@ -59,7 +60,12 @@ class App extends Component {
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     } else if(this.state.mode === "create"){
       _article = <CreateContent onSubmit={function(_title, _desc){
-        
+        this.max_content_id = this.max_content_id + 1;
+        //this.state.contents.push({id:this.max_content_id, title:_title, desc:_desc});
+        //push는 원본data 수정, concat은 수정x
+        var _contents = this.state.contents.concat({id:this.max_content_id, title:_title, desc:_desc});
+        this.setState({contents:_contents});
+        console.log(_title, _desc);
       }.bind(this)}></CreateContent>;
     }
     return (
