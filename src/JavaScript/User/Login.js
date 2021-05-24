5ff8d652cec6c4d931cc41c9133205d749d44bea
leehaejun <gowns030368@gmail.com>
2021-05-24 23:03:01 +0900

    기존프로젝트에 작업한거까지 추가

---
+++ src/JavaScript/User/Login.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import "../../CSS/User/Login.css";
+
+class Login extends Component {
+    
+    constructor(props){
+        super(props);
+      
+        const [Id, setId] = useState("")
+        const [Password, setPassword] = useState("")
+        }
+    
+      render(){
+    return (
+
+      <div className="Login">
+
+            <form className="">
+                <h1>로그인 페이지</h1>
+                <h4>
+                <label> ID </label>
+                <input type="text" placeholder="Id" required autoFocus />
+                </h4>
+                <h4>
+                <label> Password</label>
+                <input type="password" placeholder="Password" required />
+                </h4>
+                <button className="" >Login</button>
+            </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
