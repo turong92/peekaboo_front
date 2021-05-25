618c2fbeaff8b8fb78faff4d8d6cb6bfbd0c99cb
leehaejun <gowns030368@gmail.com>
2021-05-25 20:11:05 +0900

    login, signup Uiform 작업 1차. (dinner time)

--- src/JavaScript/Home/ReadContents.js
+++ src/JavaScript/Home/ReadContents.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import "../../CSS/Main/ReadContents.css";
 
 class ReadContents extends Component {
     constructor(props){
@@ -8,9 +9,9 @@ class ReadContents extends Component {
             //contents:[]
             //아래 contents는 dummy입니다.
             contents:[
-                {idx:1, id:"dummy1", insertTime:"2021-05-07", content:"새로 스믈 여듫 짜랄 맹가노니 사람마다 해여 수비니겨 날로 쑤메 뻔한킈 하고져 할따라미니라", viewCnt:999},
-                {idx:2, id:"dummy2", insertTime:"2021-05-06", content:"이런 전차로 어린 백셩이 니르고져 홇베이셔도 마참네 제 뜨들 시러펴디 몯핧 노미하니아 내 이랄 윙하야 어엿비너겨", viewCnt:999},
-                {idx:3, id:"dummy3", insertTime:"2021-05-05", content:"나랏말싸미 듕귁에 달아 문자와로 서르 사맛디 아니할쎄", viewCnt:999},
+                {idx:1, id:"dummy1", insertTime:"2021-05-07", content:"1", viewCnt:999},
+                {idx:2, id:"dummy2", insertTime:"2021-05-06", content:"2 어엿비너겨", viewCnt:999},
+                {idx:3, id:"dummy3", insertTime:"2021-05-05", content:"3", viewCnt:999},
             ]
         }
     }
@@ -24,6 +25,7 @@ class ReadContents extends Component {
             console.error(e);
         });
     }
+    
 
     //테스트할 땐 아래 함수 주석처리하고 생성자에 contents에 더미 넣어서 테스트하면 됩니다.
     //아래 코드는 server랑 붙을 때 사용합니다.
@@ -58,6 +60,15 @@ class ReadContents extends Component {
         console.log("render", list);
         return(
             <div>
+                <div>
+                    <h1>게시글 폼</h1>
+                    <h1>글작성 -> list에 정보 담고 -> 게시글 렌더링</h1>
+                    <h1>글작성시 idx는 +1, id는 받아오고, 시간받아오고, 내용</h1>
+                    <form className= "board"> 
+                        <input type="text" className="textform"></input>
+                        <button>작성하기</button>
+                    </form>
+                </div>
                 {list}
             </div>
         );
