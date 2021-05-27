0f2dffedf2e79076f6595be78b4e754fffd661aa
leehaejun <gowns030368@gmail.com>
2021-05-27 16:52:13 +0900

    home css update

--- src/JavaScript/Home/Menu.js
+++ src/JavaScript/Home/Menu.js
@@ -15,16 +15,19 @@ class Menu extends Component {
                     <div>IMG <NavLink to="/home">홈</NavLink></div>
                 </div>
                 <nav className="menuNav">
-                    <div>IMG <NavLink to="/home">홈</NavLink></div>
-                    <div>IMG <NavLink to="/">알림</NavLink></div>
-                    <div>IMG <NavLink to="/">쪽지</NavLink></div>
-                    <div>IMG <NavLink to="/">북마크</NavLink></div>
-                    <div>IMG <NavLink to="/">리스트</NavLink></div>
-                    <div>IMG <NavLink to="/">프로필</NavLink></div>
-                    <div>IMG <NavLink to="/">더보기</NavLink></div>
+                    <div className="Navlink">IMG <NavLink to="/home">홈</NavLink></div>
+                    <div className="Navlink">IMG <NavLink to="/">알림</NavLink></div>
+                    <div className="Navlink">IMG <NavLink to="/">쪽지</NavLink></div>
+                    <div className="Navlink">IMG <NavLink to="/">북마크</NavLink></div>
+                    <div className="Navlink">IMG <NavLink to="/">리스트</NavLink></div>
+                    <div className="Navlink">IMG <NavLink to="/">프로필</NavLink></div>
+                    <div className="Navlink">IMG <NavLink to="/">더보기</NavLink></div>
                 </nav>
                 <div className="BtnDiv">
                     <button className="BtnLeft"><span>트윗하기</span></button>
+                </div>
+                <div className="blank">
+                    
                 </div>
                 <div className="UserMenu">
                     유저 정보
