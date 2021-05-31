303d9ba482bff1e554a84361ebb0f4a330f6f1e3
leehaejun <gowns030368@gmail.com>
2021-05-31 22:46:41 +0900

    pull Request 1.

---
+++ src/JavaScript/User/Login.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import "../../CSS/User/Login.css";
+import UiForm from "./UiForm";
+
+
+class Login extends Component {
+  render(){
+    return (
+      <div className="Login">
+        <UiForm>
+        </UiForm>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+export default Login;
