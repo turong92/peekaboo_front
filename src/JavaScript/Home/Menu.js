bb942c778bb7d5bf7354f4a0250baf669bef5696
leehaejun <gowns030368@gmail.com>
2021-05-31 19:02:11 +0900

    model update 2

--- src/JavaScript/Home/Menu.js
+++ src/JavaScript/Home/Menu.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import {NavLink} from 'react-router-dom';
 import "../../CSS/Home/Menu.css";
+import Modal from './Modal/Modal';
 
 class Menu extends Component {
+  
+    constructor(props){
+        super(props);
+        this.state = {
+            modalOpen:false,
+        }
+    }
+    openModal = () => {
+        this.setState({ modalOpen:true})
+    }
+    closeModal = () => {
+        this.setState({ modalOpen:false})
+    }
+
+
     clickHome = () => {
         window.alert("Home");
     }
@@ -31,7 +47,23 @@ class Menu extends Component {
                         <a><NavLink to="/"><svg><path id="path4" d="m22.46 6c-0.77 0.35-1.6 0.58-2.46 0.69 0.88-0.53 1.56-1.37 1.88-2.38-0.83 0.5-1.75 0.85-2.72 1.05-0.79-0.86-1.9-1.36-3.16-1.36-2.35 0-4.27 1.92-4.27 4.29 0 0.34 0.04 0.67 0.11 0.98-3.56-0.18-6.73-1.89-8.84-4.48-0.37 0.63-0.58 1.37-0.58 2.15 0 1.49 0.75 2.81 1.91 3.56-0.71 0-1.37-0.2-1.95-0.5v0.03c0 2.08 1.48 3.82 3.44 4.21-0.36 0.1-0.74 0.15-1.13 0.15-0.27 0-0.54-0.03-0.8-0.08 0.54 1.69 2.11 2.95 4 2.98-1.46 1.16-3.31 1.84-5.33 1.84-0.34 0-0.68-0.02-1.02-0.06 1.9 1.22 4.16 1.93 6.58 1.93 7.88 0 12.21-6.54 12.21-12.21 0-0.19 0-0.37-0.01-0.56 0.84-0.6 1.56-1.36 2.14-2.23z"/></svg>더보기</NavLink></a></div>
                 </nav>
                 <div className="BtnDiv">
-                    <button className="BtnLeft"><span>트윗하기</span></button>
+                    <button className="BtnLeft" onClick={this.openModal}><span>트윗하기</span></button>
+                    <Modal open = { this.state.modalOpen } close = { this.closeModal }>
+                     <div className="boardup"> 
+                        <div className="boardupLeft"></div>
+                        <div className="boardupCenter">
+                            <div className="boardWrite" type="text" contentEditable="true" placeholder="게시글 입력">
+                            </div>
+                            <div className="read"></div>    
+                        </div>
+                        <div className="boardupBottom">
+                            <div className="boardupBottomLeft">
+                                <div role="button"></div>
+                            </div>
+                            <div className="boardupBottomRight"><button className="InputBoardBtn">트윗하기</button></div>
+                        </div>
+                     </div>
+                    </Modal>
                 </div>
                 <div className="blank">
                     
@@ -44,5 +76,6 @@ class Menu extends Component {
         );
     }
 }
+
 
 export default Menu;
