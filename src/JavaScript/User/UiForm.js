ac612d76f2a299de9c8b8126c10636ad21d8e966
leehaejun <gowns030368@gmail.com>
2021-06-01 20:17:12 +0900

    merge error update

--- src/JavaScript/User/UiForm.js
+++ src/JavaScript/User/UiForm.js
@@ -1,8 +1,6 @@
 import { render } from '@testing-library/react';
 import React, { Component, useState } from 'react';
 import "../../CSS/User/UiForm.css";
-import {Link, } from 'react-router-dom';
-
 class UiForm extends Component {
 
 render(){
@@ -22,13 +20,17 @@ render(){
                 <div className="Login">
                     <form className="loginform">
                         <div className="logintitle">PEECABOO</div>
-                            <h3>로그인</h3>
-                           <input className="email" label="email" name ="email" placeholder="이메일" type="email"/>
-                           <input className="email" label="password" name ="password" placeholder="비밀번호" type="password" />
-                        <button className="btn1">Login</button>
+                        <div className="loginbtn ">
+                            <a href="http://localhost:8080/oauth2/authorization/google" className="btn1" role="button">Facebook Login</a></div>
+                        <div className="loginbtn">
+                            <a href="http://localhost:8080/oauth2/authorization/google" className="btn2" role="button">Google Login</a></div>
+                        <div className="loginbtn">
+                            <a href="http://localhost:8080/oauth2/authorization/google" className="" role="button">Naver Login</a></div>
+                        <div className="loginbtn">
+                            <a href="http://localhost:8080/oauth2/authorization/google" className="btn4 btn-success active" role="button">Kakao Login</a></div>
                     </form>
                     <footer>
-                    <Link to="/signup">회원가입</Link>
+                        푸터 영역
                     </footer>
                 </div>
             </div>
@@ -40,3 +42,19 @@ render(){
 };
 
 export default UiForm;
+
+
+//<div className="Main">
+//<div className="Login">
+//    <form className="loginform">
+//        <div className="logintitle">PEECABOO</div>
+//            <h3>로그인</h3>
+//           <input className="email" label="email" name ="email" placeholder="이메일" type="email"/>
+//           <input className="email" label="password" name ="password" placeholder="비밀번호" type="password" />
+//        <button className="btn1">Login</button>
+//    </form>
+//    <footer>
+//    <Link to="/signup">회원가입</Link>
+//    </footer>
+//</div>
+//</div>
